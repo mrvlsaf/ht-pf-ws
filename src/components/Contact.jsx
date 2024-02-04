2306e39a6ee97e8a3667e5b53d4d89cbdc69e00e
Devraj Singh <dsdevrajsingh55555@gmail.com>
2024-02-05 02:04:36 +0530

    contact section completed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from 'react';
+import socialMediaIcon from '../assets/social_media_icon.svg'
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <div className='contact-container'>
+      <div className='work-heading'>Contact</div>
+      <div className='contact-inner-container'>
+        <div className='contact-head-container'>
+          <div className='contact-head-title'>
+            <div>Let's Work</div>
+            <div>Together</div>
+          </div>
+          <div className='contact-head-mail'>harshittewatia12@gmail.com</div>
+        </div>
+        <hr />
+        <div className='social-media-container'>
+          <span>Instagram <img src={socialMediaIcon} alt="social media icon" /></span>
+          <span>Linkedin <img src={socialMediaIcon} alt="social media icon" /></span>
+          <span>Behance <img src={socialMediaIcon} alt="social media icon" /></span>
+          <span>Medium <img src={socialMediaIcon} alt="social media icon" /></span>
+          <span>Dribble <img src={socialMediaIcon} alt="social media icon" /></span>
+        </div>
+      </div>
+    </div>
   )
 }
--- src/components/Wallpapers.jsx
+++ src/components/Wallpapers.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from 'react';
+import wp1 from '../assets/wp1.png';
+import wp2 from '../assets/wp2.png';
+import wp3 from '../assets/wp3.png';
+import wp4 from '../assets/wp4.png';
+import wp5 from '../assets/wp5.png';
+import wp6 from '../assets/wp6.png';
 
 export default function Wallpapers() {
   return (
-    <div>Wallpapers</div>
+    <div className='wallpaper-container'>
+      <div className='work-heading'>Shot on iPhone</div>
+      <div className='wallpaper-img-container'>
+        <img src={wp1} alt="wp1" />
+        <img src={wp2} alt="wp1" />
+        <img src={wp3} alt="wp1" />
+        <img src={wp4} alt="wp1" />
+        <img src={wp5} alt="wp1" />
+        <img src={wp6} alt="wp1" />
+      </div>
+    </div>
   )
 }
